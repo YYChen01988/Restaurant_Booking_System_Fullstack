4c5b2b5ecd786a094eff66fa31aaa5d25953da1b
yingying <chen.yingying.01988@gmail.com>
2018-10-18 14:56:08 +0100

    first commit

---
+++ bookingservice/src/main/java/com/example/codeclan/bookingservice/BookingserviceApplication.java
@@ -0,0 +1,12 @@
+package com.example.codeclan.bookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingserviceApplication.class, args);
+	}
+}
