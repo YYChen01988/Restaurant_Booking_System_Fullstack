f36108eff7bcdda0d5f98e230e2de23d6b198423
tannyque <tannyque1992@gmail.com>
2018-10-20 18:37:53 +0100

    Adds bookings and additional seeds

--- restaurantfrontend/src/App.js
+++ restaurantfrontend/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import NavBar from './NavBar.js';
 import CustomerContainer from './containers/customers/CustomerContainer';
+import BookingContainer from './containers/bookings/BookingContainer';
 
 class App extends Component {
 
@@ -15,6 +16,7 @@ class App extends Component {
               const url = "/customers";
               return <CustomerContainer url={url}/>
             }}/>
+            <Route exact path="/bookings" component={BookingContainer}/>
           </Switch>
         </React.Fragment>
       </Router>
---
+++ restaurantfrontend/src/components/bookings/Booking.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Booking = (props) => {
+
+	// const bookings = 
+		return (
+			<div>
+			<h1>{props.booking.party}</h1>
+		</div>
+		)
+	}
+
+export default Booking;
