57c30e835ae165232cb363db693609e7c1d1d59f
tannyque <tannyque1992@gmail.com>
2018-10-22 21:27:39 +0100

    Formats, styles and edits navbar

--- restaurantfrontend/src/NavBar.js
+++ restaurantfrontend/src/NavBar.js
@@ -8,11 +8,12 @@ const NavBar = (props) => {
         <li>
           <Link to="/">Home</Link>
         </li>
-        <li>
-          <Link to="/bookings/new">Create Booking</Link>
-        </li>
-        <li>
-          <Link to="/bookings">Bookings</Link>
+        <li className="dropdown">
+          <a className="dropbtn">Bookings<i class="fa fa-caret-down"></i></a>
+          <div className="dropdown-content">
+            <Link to="/bookings">Bookings</Link>
+            <Link to="/bookings/new">Create Booking</Link>
+          </div>
         </li>
         <li>
           <Link to="/customers">Customers</Link>
