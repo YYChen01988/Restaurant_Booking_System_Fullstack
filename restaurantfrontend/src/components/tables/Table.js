739b57bc65869d5bb6a792ec47fb718bcc1b0457
tannyque <tannyque1992@gmail.com>
2018-10-22 08:03:10 +0100

    Adds hidden capitalise method to stateless component

--- restaurantfrontend/src/components/tables/Table.js
+++ restaurantfrontend/src/components/tables/Table.js
@@ -2,12 +2,20 @@ import React from 'react';
 
 const Table = (props) => {
 
+  // capitalise(string) {
+  //   const firstCharacter = string[0].toUpperCase();
+  //   const restOfString = string.slice(1).toLowerCase();
+  //   return firstCharacter + restOfString;
+  // }
+
+  const reserved = String(props.table.reserved);
+
   const url = "/tables/" + props.table.id
   return (
     <tr>
       <td><a href={url}>{props.table.tableNumber}</a></td>
       <td>{props.table.capacity}</td>
-      <td>{String(props.table.reserved)}</td>
+      <td>{this.capitalise(reserved)}</td>
       <td>Booking goes here</td>
       {/* <td>{props.table.bookings}</td> */}
     </tr>
