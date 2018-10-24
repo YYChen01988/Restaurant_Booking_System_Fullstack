3dcb842a5db19e6352d6e12260d5e0fb893c4eda
tannyque <tannyque1992@gmail.com>
2018-10-24 16:02:05 +0100

    MVPgaa

--- restaurantfrontend/src/containers/bookings/EditBookingFormContainer.js
+++ restaurantfrontend/src/containers/bookings/EditBookingFormContainer.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import moment from 'moment';
 
 class EditBookingFormContainer extends Component {
   constructor(props){
@@ -21,12 +24,15 @@ class EditBookingFormContainer extends Component {
 
   handleSubmit(event){
     event.preventDefault();
-    console.log("submit is: ",event.target.date);
+    console.log("submit is: ",event.target.datepicker.value);
+    var formattedDate = moment(event.target.datepicker.value).format('YYYY-MM-DDTHH:mm:ss');
+    var formattedEndDate = moment(event.target.datepicker.value).add(2, 'hours').format('YYYY-MM-DDTHH:mm:ss');
     fetch(this.props.url, {
       method: 'PATCH',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
-        "date": event.target.date.value,
+        "startTime": formattedDate,
+        "endTime": formattedEndDate
       })
     })
     .then(() => {
@@ -39,7 +45,7 @@ class EditBookingFormContainer extends Component {
     .then((res) => res.json())
     .then((data) => {
       this.setState({bookings: data})
-      console.log("data is:", data.customer.name);
+      console.log("data is:", data);
     })
   }
 
@@ -48,11 +54,27 @@ class EditBookingFormContainer extends Component {
 
     return(
       <div className="form-container">
+        <p>Original booking: {moment(this.state.bookings.startTime).format('DD-MM-YY HH:mm')}</p>
         <form horizontal onSubmit={this.handleSubmit}>
           <p>Booking Customer: {this.state.bookings.customer.name} </p>
+          <p>Table Number: {this.state.bookings.table.tableNumber}</p>
+          <p>Table Capacity: {this.state.bookings.table.capacity}</p>
           <p>Change Booking Date: </p>
-          <input type="datetime-local" value={this.state.bookings.date} onChange={this.handleChange} name="date"/>
-          <button className="button" type="submit">Save Edited Details</button>
+          <DatePicker
+            className="datepicker"
+            id="datepicker"
+            value={moment(this.state.bookings.startTime).format('DD-MM-YY HH:mm')}
+            selected={this.state.startDate}
+            onChange={this.handleDate}
+            showTimeSelect
+            minTime={moment().hours(12).minutes(0)}
+            maxTime={moment().hours(22).minutes(30)}
+            timeFormat="HH:mm"
+            timeIntervals={30}
+            dateFormat="LLL"
+            timeCaption="time"
+          />
+          <button className="button" type="submit">Save Changes</button>
         </form>
       </div>
     )
